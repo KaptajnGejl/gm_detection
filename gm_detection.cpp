8635b4c5e66ba13e3c51aadc70e8f75d67e5b477
KaptajnGejl <martinfei88@gmail.com>
2018-01-03 11:43:24 +0100

    Initial commit

---
+++ gm_detection.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <iomanip>
+#include <sys/types.h>
+#include <dirent.h>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
