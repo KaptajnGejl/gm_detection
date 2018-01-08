1c90f479f725664d1cb78f98031cb548f7449338
KaptajnGejl <martinfei88@gmail.com>
2018-01-08 14:56:34 +0100

    Added base cross center functionality

--- src/func.cpp
+++ src/func.cpp
@@ -1,7 +1,9 @@
 // Libraries
 #include <iostream>
 #include <iomanip>
+#include <math.h>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 #include "func.hpp"
 
@@ -16,6 +18,10 @@ void clrscr(){
 
 	return;
 } 
+
+bool dist_cmp(corner a, corner b){
+	return a.dist < b.dist;
+}
 
 int optimal_threshold(const Mat hist)
 {
@@ -61,7 +67,7 @@ int optimal_threshold(const Mat hist)
 		
 	}
 	
-	return T+10;
+	return T-30;
 }
 
 Mat hist(Mat img, bool show_image) 
@@ -99,5 +105,145 @@ Mat hist(Mat img, bool show_image)
 
 }
 
+corner global_center(vector<corner> corners){
+	
+	corner temp;
+
+	
+	if(corners.size() > 0){
+
+		for(unsigned int i = 0; i<corners.size(); i++){
+
+			temp.pos.x += corners[i].pos.x;
+			temp.pos.y += corners[i].pos.y; 
+
+		}
+
+		temp.pos.x /= corners.size();
+		temp.pos.y /= corners.size();
+	}
+
+	return temp;
+}
+
+corner cross_center(Mat& img_cor, vector<corner> corners){
+
+	corner center, c_center;
+	vector<corner> result;
+	char count = 0;
+	bool first=false;
+	unsigned int i = 0,index = 0;
+	float avg_err = 0, best_err = 10000;
+
+	center = global_center(corners);
+
+	for(i = 0; i<corners.size(); i++){
+
+		corners[i].dist = sqrt(pow(corners[i].pos.x-center.pos.x,2)+pow(corners[i].pos.y-center.pos.y,2));
+
+		//cout << "x: " << corners[i].pos.x << " y: " << corners[i].pos.y << " dist: " << corners[i].dist << endl;
+
+	}
+	cout << endl;
+
+	sort(corners.begin(),corners.end(),dist_cmp);
+	/*
+	for(i = 0; i<corners.size(); i++){
+		cout << "x: " << corners[i].pos.x << " y: " << corners[i].pos.y << " dist: " << corners[i].dist << endl;
+	}
+	*/
+
+	while(corners.size()>4){
+
+		cout << endl << "Corners(" << corners.size() << "): " << endl;
+		for(i = 0; i<corners.size(); i++){
+			cout << "x: " << corners[i].pos.x << " y: " << corners[i].pos.y << " dist: " << corners[i].dist << endl;
+		}
+
+		for(i = 1; i<corners.size(); i++){
+			if(abs(corners[i].dist-corners[i-1].dist)<=10 && first == false){
+				count++;
+				first = true;
+				index = i-1;
+				avg_err += abs(corners[i].dist-corners[i-1].dist);
+			}
+			else if(abs(corners[i].dist-corners[index].dist)<=10 && first==true){
+				count++;
+				avg_err += abs(corners[i].dist-corners[i-1].dist);
+			}
+			else
+			{
+				count=0;
+				first = false;
+				avg_err = 0;
+			}
+
+			if(count==3){
+				avg_err /= 4; 
+				if(avg_err<best_err){
+					best_err = avg_err;
+
+					result.clear();
+					for(unsigned int j = index; j<index+4; j++){
+						result.push_back(corners[j]);
+					}
+					
+					cout << "Best error: " << best_err << ", Index:" << index << endl;
+				}
+				first = false; 
+			}
+		}
+
+		corners.pop_back();	
+		center = global_center(corners);
+
+		for(i = 0; i<corners.size(); i++){
+			corners[i].dist = sqrt(pow(corners[i].pos.x-center.pos.x,2)+pow(corners[i].pos.y-center.pos.y,2));
+			//cout << "x: " << corners[i].pos.x << " y: " << corners[i].pos.y << " dist: " << corners[i].dist << endl;
+		}
+
+		sort(corners.begin(),corners.end(),dist_cmp);
+	}
+
+	if(result.size()>0){
+
+		for(unsigned int j = 0; j<result.size(); j++){
+			c_center.pos.x += result[j].pos.x;
+			c_center.pos.y += result[j].pos.y;
+			circle(img_cor,Point(result[j].pos.x,result[j].pos.y),3,Scalar(0,255,0),-1,0);
+		}
+
+		c_center.pos.x /= 4;
+		c_center.pos.y /= 4;
+
+		//for(i = 0; i<corners.size();i++){
+			
+		//}
+
+		cout << endl << "Corners(" << corners.size() << "): " << endl;
+		for(i = 0; i<corners.size(); i++){
+			cout << "x: " << corners[i].pos.x << " y: " << corners[i].pos.y << " dist: " << corners[i].dist << endl;
+		}
+
+		cout << "Done!" << endl;
+
+		return c_center;
+	}
+	else
+	{
+
+		for(i = 0; i<corners.size();i++){
+			circle(img_cor,Point(corners[i].pos.x,corners[i].pos.y),3,Scalar(0,255,0),-1,0);
+		}
+
+		cout << endl << "Corners(" << corners.size() << "): " << endl;
+		for(i = 0; i<corners.size(); i++){
+			cout << "x: " << corners[i].pos.x << " y: " << corners[i].pos.y << " dist: " << corners[i].dist << endl;
+		}
+
+		return center;
+
+	}
+}
 
 
